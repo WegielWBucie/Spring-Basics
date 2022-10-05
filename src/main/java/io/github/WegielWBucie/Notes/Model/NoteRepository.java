6f2cc811681d81e220d151b938ddffed55cb6355
WegielWBucie <pondroszejset@gmail.com>
2022-10-05 18:03:54 +0200

    RepositoryRestResource Annotation

--- src/main/java/io/github/WegielWBucie/Notes/Model/NoteRepository.java
+++ src/main/java/io/github/WegielWBucie/Notes/Model/NoteRepository.java
@@ -1,4 +1,9 @@
 package io.github.WegielWBucie.Notes.Model;
 
-interface NoteRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface NoteRepository extends JpaRepository<Note, Long> {
+
 }
