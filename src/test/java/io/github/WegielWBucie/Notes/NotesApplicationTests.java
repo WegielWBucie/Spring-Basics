e45011b714f9e39d7ef9e44a4968fe8956fbe861
WegielWBucie <pondroszejset@gmail.com>
2022-10-05 17:04:02 +0200

    test

---
+++ src/test/java/io/github/WegielWBucie/Notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.WegielWBucie.Notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
