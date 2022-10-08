a06c4bb63de17354c936ad1a3f3599b9ba90c131
WegielWBucie <pondroszejset@gmail.com>
2022-10-08 02:25:06 +0200

    DeleteMapping

--- src/main/java/io/github/WegielWBucie/Notes/Controller/NoteController.java
+++ src/main/java/io/github/WegielWBucie/Notes/Controller/NoteController.java
@@ -5,6 +5,7 @@ import io.github.WegielWBucie.Notes.Model.NoteRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -42,6 +43,7 @@ class NoteController {
     @PutMapping(path = "/notes/{ID}")
     ResponseEntity<?> editNote(@PathVariable @Valid final Long ID) {
 
+        logger.warn("Edit mode:");
         return null;
     }
 
@@ -54,8 +56,17 @@ class NoteController {
     }
 
     @DeleteMapping(path = "/notes/{ID}")
-    ResponseEntity<?> deleteNote(@PathVariable Long ID) {
+    ResponseEntity<?> deleteNote(@PathVariable @Valid Long ID) {
 
-        return null;
+        try {
+            noteRepository.deleteById(ID);
+            logger.warn("Removing task: " + ID);
+
+            return ResponseEntity.noContent().build();
+        }
+        catch(EmptyResultDataAccessException emptyResultDataAccessException) {
+            logger.error("No note with ID " + ID + " exists!");
+            return ResponseEntity.notFound().build();
+        }
     }
 }
