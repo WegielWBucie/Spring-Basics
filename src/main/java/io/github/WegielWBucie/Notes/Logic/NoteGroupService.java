7de0d7a1d401237ec37a7b5b3bdf620bb7473f70
WegielWBucie <pondroszejset@gmail.com>
2023-04-01 05:48:12 +0200

    RequestScope annotation.

--- src/main/java/io/github/WegielWBucie/Notes/Logic/NoteGroupService.java
+++ src/main/java/io/github/WegielWBucie/Notes/Logic/NoteGroupService.java
@@ -2,35 +2,45 @@ package io.github.WegielWBucie.Notes.Logic;
 
 import io.github.WegielWBucie.Notes.Model.NoteGroup;
 import io.github.WegielWBucie.Notes.Model.NoteGroupRepository;
+import io.github.WegielWBucie.Notes.Model.NoteRepository;
 import io.github.WegielWBucie.Notes.Model.Projection.GroupReadModel;
 import io.github.WegielWBucie.Notes.Model.Projection.GroupWriteModel;
+import io.github.WegielWBucie.Notes.NoteConfigurationProperties;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@RequestScope
 public class NoteGroupService {
-    /*
-        Intermediate level between
-        repository & controller.
+    /**
+     *    Intermediate level between
+     *    repository & controller.
      */
+    private NoteGroupRepository noteGroupRepository;
+    private NoteRepository noteRepository;
 
-    private NoteGroupRepository repository;
-
-    NoteGroupService(final NoteGroupRepository repository) {
-        this.repository = repository;
+    NoteGroupService(final NoteGroupRepository repository, final NoteConfigurationProperties config) {
+        this.noteGroupRepository = repository;
     }
 
     public GroupReadModel createGroup(final GroupWriteModel source) {
-        NoteGroup result = repository.save(source.toGroup());
+        NoteGroup result = noteGroupRepository.save(source.toGroup());
         return new GroupReadModel(result);
     }
 
     public List<GroupReadModel> readAll() {
-        return repository.findAll().stream()
+        return noteGroupRepository.findAll().stream()
                 .map(GroupReadModel::new)
                 .collect(Collectors.toList());
+    }
+
+    public void toggleGroup(final Long groupID) {
+        NoteGroup result = noteGroupRepository.findByID(groupID)
+                .orElseThrow(() -> new IllegalArgumentException("NoteGroup wihth given ID not found."));
+        result.setTitle("<Closed> " + result.getTitle());
     }
 
 }
