ca3cb791643aee57899c8aa784488782ab772a10
WegielWBucie <pondroszejset@gmail.com>
2023-06-13 15:44:03 +0200

    LoggerFilter class.

---
+++ src/main/java/io/github/WegielWBucie/Notes/Controller/LoggerFilter.java
@@ -0,0 +1,27 @@
+package io.github.WegielWBucie.Notes.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Order
+class LoggerFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerFilter.class);
+
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+        if(servletRequest instanceof HttpServletRequest httpRequest) {
+            logger.info("[doFilter] " + httpRequest.getMethod() + " " + httpRequest.getRequestURI());
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
